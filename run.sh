cf2af93daffef6562bb71dcd0f9e145ff185c9b1
JC <user@JCs-MacBook-Pro.local>
2025-03-15 03:58:21 +1100

    update

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ clear
 pip3 install pygame numpy
 
 
+python3 ./src/main.py
+
 
 
 printf "\n\n[SHELL] Program exit code $?\n\n"
--- src/main.py
+++ src/main.py
@@ -1,10 +1,40 @@
 import sys;
 import pygame as pg;
 import numpy as np;
+import map;
+import player;
+import settings;
+
+xRes = 800;
+yRes = 600;
 
 
 def main():
+    pg.init();
+
+    screen = pg.display.set_mode((xRes, yRes));
+    pg.display.set_caption("Raycaster");
+
+    running = True;
+    while running:
+        for event in pg.event.get():
+
+            if event.type == pg.QUIT:
+                running = False;
+            
+            elif event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    running = False;
+
+
+
+
+
+
+    pg.quit();
     return 0;
+
+
 
 
 
